345e3d650833c86de62b52b14b934a2e60f4e045
Tazeem Khan <tazeemk329@gmail.com>
2025-03-10 15:27:12 +0530

    Delete VideoLibraryProject directory

--- VideoLibraryProject/src/main/java/com/nt/repository/ICategoryMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.nt.entity.Category;
-
-public interface ICategoryMongoRepository extends MongoRepository<Category, Integer> {
-
-}
